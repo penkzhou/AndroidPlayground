c55a44d8da87fe109b7919412353cb69cd50cf60
zhoupeng05 <zhoupeng05@meituan.com>
2019-08-26 17:32:57 +0800

    add playlist

--- app/src/main/java/com/penkzhou/projects/android/playground/activity/ExoPlayer2Activity.kt
+++ app/src/main/java/com/penkzhou/projects/android/playground/activity/ExoPlayer2Activity.kt
@@ -2,18 +2,16 @@ package com.penkzhou.projects.android.playground.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.exoplayer2.ui.PlayerView
 import com.penkzhou.projects.android.playground.R
+import kotlinx.android.synthetic.main.activity_exo_player_2.*
 
 class ExoPlayer2Activity : AppCompatActivity() {
     lateinit var playerHolder: PlayerHolder
     val playerState = PlayerState()
-    lateinit var exo_player_view: PlayerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exo_player_2)
-        exo_player_view = findViewById(R.id.exo_player_view)
         playerHolder = PlayerHolder(this, exo_player_view, playerState)
     }
 
